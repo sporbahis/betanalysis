9e22dbc44ea5dd36c8058d0c87a2f1721df82ae8
spor.bahis <spor.bahis.oran@gmail.com>
2018-08-13 00:32:18 +0300

    Yapımda Aşaması

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from flask import (Flask, render_template)
 app = Flask(__name__)
 
 
-
+@app.route('/')
+def index():
+    # Use a list here so we can do "if posts" efficiently in the template.
+    return render_template('index.html')
 
 
 def main():
